83c65f2d8d9cc4efa750dabbcaf7a979853f1a26
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-22 22:20:03 -0300

    Update metodos-arreglos.js
    reduce

--- metodos-arreglos.js
+++ metodos-arreglos.js
@@ -1,4 +1,5 @@
-const arr = [5, 1, 2, 3, 4];
+//FILTER
+const arr = [-5, -1, -2, -3, -4];
 
 const r = arr.filter((el, i) => {
   console.log(i);
@@ -6,7 +7,7 @@ const r = arr.filter((el, i) => {
 });
 
 console.log(r);
-
+//MAP
 const mapped = arr.map((el) => `<h1>${el}</h1>`);
 console.log(mapped);
 
@@ -20,3 +21,24 @@ const users = [
 const mappedUser = users.map((user) => `<h1>${user.name}</h1>`);
 
 console.log(mappedUser);
+
+//REDUCE
+
+//const r1 = arr.reduce((acc, el) => acc + el, 0);
+
+//const getMax = (a, b) => Math.max(a, b);
+//VER CUANDO UTILIZAR EL SEGUNDO ARGUMENTO DE REDUCE
+//const r1 = arr.reduce(getMax, 0);
+//const r1 = users.reduce(
+//(acc, el) => `${acc === "" ? "" : `${acc}, `}${el.name}`,
+//""
+//);
+
+const r1 = users.reduce((acc, el) => {
+  if (el.id < 2) {
+    return acc;
+  }
+  return acc.concat(el);
+}, []);
+
+console.log(r1);
