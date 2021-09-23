f1b65f885b2bfd45b3f2d15b66b07b2171d8a9dd
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-22 22:02:39 -0300

    Update metodos-arreglos.js
    map

--- metodos-arreglos.js
+++ metodos-arreglos.js
@@ -6,3 +6,17 @@ const r = arr.filter((el, i) => {
 });
 
 console.log(r);
+
+const mapped = arr.map((el) => `<h1>${el}</h1>`);
+console.log(mapped);
+
+const users = [
+  { id: 1, name: "Chanchito feliz" },
+  { id: 2, name: "Chanchito triste" },
+  { id: 3, name: "Chanchito emocionado" },
+  { id: 4, name: "Guillermo" },
+];
+
+const mappedUser = users.map((user) => `<h1>${user.name}</h1>`);
+
+console.log(mappedUser);
