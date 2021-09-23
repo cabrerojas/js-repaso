c7ba75dcc59430663979b9c5e203722c6ed2abf2
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-22 21:52:26 -0300

    Create rest.js
    rest operator

---
+++ rest.js
@@ -0,0 +1,28 @@
+// ... siempre al final
+const rest = (a, b, ...argumentos) => {
+  console.log(a);
+  console.log(argumentos);
+};
+
+//rest(1, 2, 3,4,5,6);
+
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+//const { a, b, ...restobj } = obj;
+
+//console.log(a, b, restobj);
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+const [a, b, ...r] = arr;
+
+const useState = () => ["valor", () => {}];
+
+const [valor, setValor] = useState();
+
+console.log(valor, setValor);
